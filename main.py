02872e952374f110e29c0d83b68a4438a60bcb29
fyzzy1943 <fyzzy1943@gmail.com>
2016-08-17 14:38:16 +0800

    final

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time, os
 import helper.sqlite as db
 import helper.xml as x
 
 db.init()
 
-x.section = 'hdsky'
-x.build()
-x.execute()
+while True:
+    print('mission start.')
+    x.section = 'hdsky'
+    x.build()
+    x.execute()
 
-x.section = 'cmct'
-x.build()
-x.execute()
+    x.section = 'cmct'
+    x.build()
+    x.execute()
 
-print('done.')
+    print('mission complete. sleep 5min.')
+    time.sleep(60*5)
+os.system('pause')
