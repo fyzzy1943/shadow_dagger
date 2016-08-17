12538894edd6f47547f12c56e9b7be1ddb0acd23
fyzzy1943 <fyzzy1943@gmail.com>
2016-08-17 12:18:11 +0800

    final

--- helper/sqlite.py
+++ helper/sqlite.py
@@ -2,18 +2,6 @@ import sqlite3, os
 
 db_file = os.path.join(os.path.abspath('.'), 'torrent.db')
 
-def test():
-    conn = sqlite3.connect('torrent.db')
-    cursor = conn.cursor()
-
-    cursor.execute('SELECT * FROM hdsky')
-
-    values = cursor.fetchall()
-
-    print(values)
-    cursor.close()
-    conn.close()
-
 def init():
     if os.path.exists(db_file):
         return
@@ -21,18 +9,50 @@ def init():
     conn = sqlite3.connect(db_file)
     cursor=conn.cursor()
     cursor.execute(
-        r'CREATE TABLE sky(id INT PRIMARY KEY,title VARCHAR(255),link VARCHAR(255),author VARCHAR(255),category VARCHAR(255),enclosure VARCHAR(255),date VARCHAR(255))')
+        r'CREATE TABLE sky(title VARCHAR(255),link VARCHAR(255),author VARCHAR(255),category VARCHAR(255),enclosure VARCHAR(255),date VARCHAR(255))')
     cursor.close()
     conn.commit()
     conn.close()
 
     print('init successful, db file at', db_file)
 
-def insert():
+def insert(title, link, author, category, enclosure, date):
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
     cursor.execute(
-        r'CREATE TABLE sky(id INT PRIMARY KEY,title VARCHAR(255),link VARCHAR(255),author VARCHAR(255),category VARCHAR(255),enclosure VARCHAR(255),date VARCHAR(255))')
+        r'INSERT INTO sky VALUES (?,?,?,?,?,?)', (title, link, author, category, enclosure, date))
+    row = cursor.rowcount
     cursor.close()
     conn.commit()
     conn.close()
+
+    return row
+
+def has(title):
+    if title=='' or title is None:
+        return False
+
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    cursor.execute(r'SELECT rowid FROM sky WHERE title=?', (title,))
+    value = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    if len(value)>0:
+        return True
+    else:
+        return False
+
+def getID(title):
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    cursor.execute(r'SELECT rowid FROM sky WHERE title=?', (title,))
+    value = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    if len(value)>0:
+        return value[0][0]
+    else:
+        return False
--- main.py
+++ main.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from helper.getConfig import config
-url = config('main', 'url')
-print(url)
-
-from urllib import request
-
-# with request.urlopen(url) as f:
-#     data = f.read()
-#     print('Status:', f.status, f.reason)
-#     for k, v in f.getheaders():
-#         print(k, v)
-#     xml = data.decode('utf-8')
-
-
-# import xml.etree.ElementTree
-# xml = xml.etree.ElementTree.parse('a.xml')
-# movies = xml.getiterator('item')
-# print(len(movies))
-# for movie in movies:
-    # print(movie.find('title').text)
-    # print('link: '+movie.find('link').text)
-    # print('author: '+movie.find('author').text)
-    # print('category: '+movie.find('category').text)
-    # print(movie.find('enclosure').attrib['url'])
-    # print(movie.find('pubDate').text)
-
-# from helper.sqlite import test
-# test()
 import helper.sqlite as db
+import helper.url as url
+
 db.init()
+# xml = url.getXML()
+# print(xml)
+
+
+import xml.etree.ElementTree
+xml = xml.etree.ElementTree.parse('a.xml')
+movies = xml.getiterator('item')
+for movie in movies:
+    title = movie.find('title').text
+    link = movie.find('link').text
+    author = movie.find('author').text
+    category = movie.find('category').text
+    enclosure = movie.find('enclosure').attrib['url']
+    date = movie.find('pubDate').text
+
+    if db.has(title):
+        print('done.')
+        continue
+    else:
+        db.insert(title, link, author, category, enclosure, date)
+        url.download(enclosure, title)
+
