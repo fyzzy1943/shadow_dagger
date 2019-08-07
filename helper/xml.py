b161f9abb015dbe6a60d80a806628fe70899ffd8
fyzzy1943 <fyzzy1943@gmail.com>
2019-08-07 17:58:35 +0800

    wip.

--- helper/xml.py
+++ helper/xml.py
@@ -1,26 +1,49 @@
-import urllib.request, os, time
+import os, time
 import helper.sqlite as db
+from urllib import request
 
 from helper.getConfig import config
 
 section = 'default'
 
+
 def build():
     url = config(section, 'url')
 
-    with urllib.request.urlopen(url) as f:
+    # print(url)
+    req = request.Request(url)
+    req.add_header('User-Agent',
+                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36')
+    with request.urlopen(req) as f:
         data = f.read()
-        # print('Status:', f.status, f.reason)
-        if f.status!=200:
+        print('Request Status:', f.status, f.reason)
+        # exit
+        if f.status != 200:
             print('wrong code:', f.status)
 
         with open('torrent.xml', 'w', encoding='utf-8') as file:
             file.write(data.decode('utf-8'))
 
+
 def download(url, title):
-    t_name = config(section, 'name')+'_'+str(db.getID(title))+'.torrent'
+    t_name = config(section, 'name') + '_' + str(db.getID(title)) + '.torrent'
     fullname = os.path.join(config(section, 'dir'), t_name)
-    urllib.request.urlretrieve(url, fullname)
+
+    req = request.Request(url)
+    req.add_header('User-Agent',
+                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36')
+
+    with request.urlopen(req) as f:
+        data = f.read()
+        print('Download Status:', f.status, f.reason)
+        if f.status != 200:
+            print('wrong code:', f.status)
+
+        with open(fullname, 'wb') as file:
+            file.write(data)
+
+    # request.urlretrieve(url, fullname)
+
 
 def execute():
     import xml.etree.ElementTree
@@ -33,8 +56,9 @@ def execute():
         if db.has(title):
             continue
         else:
-            print('a new %s torrent: %s'% (section, title))
+            print('a new %s torrent: %s' % (section, title))
             with open('torrent.log', 'a') as file:
-                file.write('a new %s torrent: %s @ %s\n'% (section, title, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+                file.write('a new %s torrent: %s @ %s\n' % (
+                section, title, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
             db.insert(title, link, section, enclosure)
             download(enclosure, title)
